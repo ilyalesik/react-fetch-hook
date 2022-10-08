12ace920ec16bc19d7c8c1a7b1bbc48da64a3571
Christian Herrero <c.herrerotinoco@gmail.com>
2022-10-08 16:38:53 +0200

    feat-> conditional result promise

--- usePromise.js
+++ usePromise.js
@@ -12,6 +12,14 @@ var flattenInput = require("./utils/flattenInput");
  */
 function usePromise(callFunction) {
   var inputs = Array.prototype.slice.call(arguments, [1]); // optiones
+    //if abort option is activated
+    let controller;
+  if (inputs.abortController) {
+    controller = new AbortController();
+    // save reference class: signal = controller.signal
+    inputs[signal] = controller.signal
+  }
+
   var state = React.useState({
     isLoading: !!callFunction,
   });
@@ -19,35 +27,29 @@ function usePromise(callFunction) {
   // Check if exist abort controller (options or special)
   //if ((o && o?.abortController) || (s && s?.abortController)) {
   // Init AbortController Class: controller = new AbortController();
-  let controller;
-  let signal;
-  if (inputs.AbortController) {
-    controller = new AbortController();
-    // save reference class: signal = controller.signal;
-    signal = controller.signal;
-  }
+
+  
 
   React.useEffect(function () {
     if (!callFunction) {
       return;
     }
-    !state[0].isLoading && state[1]({ data: state[0].data, isLoading: true });
+    !state[0].isLoading && state[1]({ data: state[0].data, isLoading: true, abort: () => {
+      controller.abort();
+       }, });
     callFunction
-      .apply(null, { ...inputs, signal })
+      .apply(null, inputs)
       .then(function (data) {
         state[1]({
           data: data,
           isLoading: false,
-          abort: () => {
-            controller.abort();
-          },
+
         });
       })
       .catch(function (error) {
         state[1]({
           error: error,
-          isLoading: false,
-          abort: () => {},
+          isLoading: false
         });
       });
   }, flattenInput(inputs));
