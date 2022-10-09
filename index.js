e76068bffc100968760de7775ca87a520ccb13ab
Christian Herrero <c.herrerotinoco@gmail.com>
2022-10-09 10:11:14 +0200

    fix -> eslint issues

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var usePromise = require("./usePromise");
+var usePromise = require('./usePromise')
 
-function UseFetchError(status, statusText, message, fileName, lineNumber) {
-  var instance = new Error(message, fileName, lineNumber);
-  instance.name = "UseFetchError";
-  instance.status = status;
-  instance.statusText = statusText;
-  Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+function UseFetchError (status, statusText, message, fileName, lineNumber) {
+  var instance = new Error(message, fileName, lineNumber)
+  instance.name = 'UseFetchError'
+  instance.status = status
+  instance.statusText = statusText
+  Object.setPrototypeOf(instance, Object.getPrototypeOf(this))
   if (Error.captureStackTrace) {
-    Error.captureStackTrace(instance, UseFetchError);
+    Error.captureStackTrace(instance, UseFetchError)
   }
-  return instance;
+  return instance
 }
 
 UseFetchError.prototype = Object.create(Error.prototype, {
@@ -17,42 +17,34 @@ UseFetchError.prototype = Object.create(Error.prototype, {
     value: Error,
     enumerable: false,
     writable: true,
-    configurable: true,
+    configurable: true
+  }
+})
+
+Object.setPrototypeOf(UseFetchError, Error)
+
+function useFetch (
+  path,
+  options,
+  specialOptions
+) {
+  var blocked = ((specialOptions && specialOptions.depends) ||
+      (options && options.depends) || [])
+    .reduce(function (acc, dep) { return acc || !dep }, false)
+  return usePromise(!blocked && function (p, o, s) {
+    return fetch(p, o)
+      .then((s && s.formatter) || (o && o.formatter) || function (response) {
+        if (!response.ok) {
+          throw new UseFetchError(
+            response.status,
+            response.statusText,
+            'Fetch error'
+          )
+        }
+        return response.json()
+      })
   },
-});
-
-Object.setPrototypeOf(UseFetchError, Error);
-
-function useFetch(path, options, specialOptions) {
-  var blocked = (
-    (specialOptions && specialOptions.depends) ||
-    (options && options.depends) ||
-    []
-  ).reduce(function (acc, dep) {
-    return acc || !dep;
-  }, false);
-  return usePromise(
-    !blocked &&
-      function (p, o, s) {
-        return fetch(p, o).then(
-          (s && s.formatter) ||
-            (o && o.formatter) ||
-            function (response) {
-              if (!response.ok) {
-                throw new UseFetchError(
-                  response.status,
-                  response.statusText,
-                  "Fetch error"
-                );
-              }
-              return response.json();
-            }
-        );
-      },
-    path,
-    options || {},
-    specialOptions || {}
-  );
+  path, options || {}, specialOptions || {})
 }
 
-module.exports = useFetch;
+module.exports = useFetch
--- usePromise.js
+++ usePromise.js
@@ -1,55 +1,56 @@
-var React = require("react");
+var React = require('react')
 
-var flattenInput = require("./utils/flattenInput");
+var flattenInput = require('./utils/flattenInput')
 
 /**
  * Hook that uses the callFunction to return state
- * @param {()=>{}} callFunction , return fetch function, depends on formatter boolean option
+ * @param {()=>{}} callFunction return fetch function,
+ * depends on formatter boolean option.
  * @returns {Object} {data, isLoading, error, abort}
  */
-function usePromise(callFunction) {
-  var inputs = Array.prototype.slice.call(arguments, [1]); // options
+function usePromise (callFunction) {
+  var inputs = Array.prototype.slice.call(arguments, [1]) // options
 
-  let controller;
+  var controller
   if (inputs[1].abortController) {
-    controller = new AbortController();
-    inputs[1].signal = controller.signal;
+    controller = new AbortController()
+    inputs[1].signal = controller.signal
   }
   /**
    * if abortController is setted, returns the Function to abort
    * the fetch if not returns null
    * @returns {()=>{}}
    */
-  const abortFn = () => (controller ? controller.abort() : null);
+  var abortFn = function () { return controller ? controller.abort() : null }
 
   var state = React.useState({
-    isLoading: !!callFunction,
-  });
+    isLoading: !!callFunction
+  })
 
   React.useEffect(function () {
     if (!callFunction) {
-      return;
+      return
     }
     !state[0].isLoading &&
-      state[1]({ data: state[0].data, isLoading: true, abort: abortFn });
+      state[1]({ data: state[0].data, isLoading: true, abort: abortFn })
     callFunction
       .apply(null, inputs)
       .then(function (data) {
         state[1]({
           data: data,
           isLoading: false,
-          abort: abortFn,
-        });
+          abort: abortFn
+        })
       })
       .catch(function (error) {
         state[1]({
           error: error,
-          isLoading: false,
-        });
-      });
-  }, flattenInput(inputs));
+          isLoading: false
+        })
+      })
+  }, flattenInput(inputs))
 
-  return state[0];
+  return state[0]
 }
 
-module.exports = usePromise;
+module.exports = usePromise
